ee56df23c65ba83a603b4fa215508597ceeb9d95
kalose1017 <leejiho1369@naver.com>
2024-03-25 21:14:28 +0900

    [21LJH64]3주차 과제

--- HW/HW03/Lecture03-HW(LJH,202127064)/Lecture03-HW(LJH,202127064)/Lecture03-HW(LJH,202127064).cpp
+++ HW/HW03/Lecture03-HW(LJH,202127064)/Lecture03-HW(LJH,202127064)/Lecture03-HW(LJH,202127064).cpp
@@ -18,7 +18,7 @@ int main() {
     int keycode = 0;
     std::cout << "학번 : 202127064\n";
     std::cout << "이름 : 이지호\n\n";
-    while (keycode >= 0 && keycode <= 64) // keycode가 0부터 64사이 값일때만 반복 실행
+    while (keycode >= 0 && keycode < 64)
     {
         std::cout << "화면에 그릴 물체 코드를 입력하세요 : ";
         std::cin >> keycode;
@@ -29,20 +29,20 @@ int main() {
 
         // 2진수에서 2진수 1이 있는 자리에 색깔 출력
         std::cout << "2진수 값에 색깔 입히기!! : ";
-        for (int i = 0; i < binaryString.size(); ++i) {
+        for (int i = 2; i < 8; ++i) {
             if (binaryString[i] == '1') {
                 if (i == 7)
-                    std::cout << "\x1b[37m"; // 흰색으로 출력 (2진수 1의 자리)
+                    std::cout << "\x1b[37m"; // 흰색으로 출력
                 else if (i == 6)
-                    std::cout << "\x1b[31m"; // 빨강색으로 출력 (2진수 10의 자리)
+                    std::cout << "\x1b[31m"; // 빨강색으로 출력
                 else if (i == 5)
-                    std::cout << "\x1b[32m"; // 녹색으로 출력 (2진수 100의 자리)
+                    std::cout << "\x1b[32m"; // 녹색으로 출력
                 else if (i == 4)
-                    std::cout << "\x1b[33m"; // 노란색으로 출력 (2진수 1000의 자리)
+                    std::cout << "\x1b[33m"; // 노란색으로 출력
                 else if (i == 3)
-                    std::cout << "\x1b[36m"; // 청록색으로 출력 (2진수 10000의 자리)
+                    std::cout << "\x1b[36m"; // 청록색으로 출력
                 else if (i == 2)
-                    std::cout << "\x1b[35m"; // 보라색으로 출력 (2진수 100000의 자리)
+                    std::cout << "\x1b[35m"; // 보라색으로 출력
             }
             std::cout << binaryString[i] << "\x1b[37m"; // 출력 후 재입력칸 흰색으로 출력
         }
